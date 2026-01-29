d1959c22462e7c334845a60bb82bd01a1c0859bd
letquit <1446444589@qq.com>
2026-01-29 21:08:01 +0800

    Switch Type Matching

--- Assets/Scripts/15-SwitchTypeMatching/SwitchTypeMatching.cs
+++ Assets/Scripts/15-SwitchTypeMatching/SwitchTypeMatching.cs
@@ -1,16 +1,69 @@
+using System;
 using UnityEngine;
 
 public class SwitchTypeMatching : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        IAttackable attackable = new Player()
+        {
+            health = 100
+        };
+
+        // if (attackable is Player) { }
+        // if (attackable is Enemy) {}
+        // if (attackable is RedBarrel) {}
+        switch (attackable)
+        {
+            case Player player when player.health > 50:  
+                Debug.Log("Health 50");
+                player.PlayerSayHello();
+                break;
+            case Player player:  
+                player.PlayerSayHello();
+                break;
+            case Enemy enemy:
+                break;
+            case RedBarrel barrel:
+                break;
+            default:
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public class Player : Unit, IAttackable, IMovable, IHasInventory
+    {
+        public int health;
+        
+        public void Damage()
+        {
+            Debug.Log("Player damage");
+        }
+
+        public void PlayerSayHello()
+        {
+            Debug.Log("Player say hello");
+        }
+    }
+
+    public class Enemy : Unit, IAttackable, IMovable { }
+    
+    public class RedBarrel : Unit, IAttackable, IMovable { }
+
+    public class Unit
     {
         
     }
+    
+    public interface IAttackable
+    {
+        public void Damage()
+        {
+            Debug.Log("Damage");
+        }
+    }
+    
+    public interface IMovable { }
+    
+    public interface IHasInventory { }
 }
