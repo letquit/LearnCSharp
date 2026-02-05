8ba01bcc3e666aedec2f8b1343a8d9a75d61f7f9
letquit <1446444589@qq.com>
2026-02-05 09:43:24 +0800

    Preprocessor Directives

---
+++ Assets/Scripts/Advanced/18-PreprocessorDirectives/PreprocessorDirectives.cs
@@ -0,0 +1,51 @@
+// #define TESTING
+#define TESTING2
+
+using System;
+using UnityEngine;
+
+// #pragma warning disable 0168
+public class PreprocessorDirectives : MonoBehaviour
+{
+    #region FUNCTIONS
+    private void Start()
+    {
+        bool testing = true;
+        if (testing)
+        {
+            Debug.Log("Testing");
+        }
+        
+#if TESTING
+        Debug.Log("Testing");
+#elif TESTING2
+        Debug.Log("Testing 2");
+#else
+        Debug.Log("Not Testing");
+#endif
+      
+#if DEBUG
+        Debug.Log("Debugging");
+#endif
+        
+#if RELEASE
+        Debug.Log("Releasing");
+#endif
+      
+#if NET6_0_OR_GREATER
+        Debug.Log("Net 6.0 or greater");
+#endif
+        
+#if !Net8_0
+        Debug.Log("Net 8.0");
+#error Some error
+#endif
+
+#warning This is a warning
+#pragma warning disable
+        int i;
+#pragma warning restore
+        int b;
+    }
+    #endregion
+}
