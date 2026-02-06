1714d82d9ae52f00a121691db93081cacceca0a9
letquit <1446444589@qq.com>
2026-02-06 21:26:39 +0800

    Unsafe, Pointers

---
+++ Assets/Scripts/Advanced/32-UnsafePointers/UnsafePointers.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+// public unsafe class UnsafePointers : MonoBehaviour
+public class UnsafePointers : MonoBehaviour
+{
+    // private unsafe void Start()
+    private void Start()
+    {
+        // unsafe
+        {
+            // int* intArrayPtr = stackalloc int[10];
+            // intArrayPtr += 5;
+            // // for (int i = 0; i < 20; i++)
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     Debug.Log(intArrayPtr[i]);
+            // }
+            // Debug.Log(((IntPtr)intArrayPtr).ToString("x"));
+            //
+            // GameData gameData = new GameData();
+            // GameData* gameDataPtr = &gameData;
+            // gameDataPtr->coins = 12;
+            // gameDataPtr->isGameOver = false;
+            //
+            // int a = 5;
+            // PowerTwo(&a);
+            // Debug.Log(a);
+            //
+            // byte[] bytes = new byte[] { 1, 2, 3, 4, 5 };
+            // fixed (byte* bytePtr = bytes)
+            // {
+            //     
+            // }
+        }
+    }
+
+    public struct GameData
+    {
+        public int coins;
+        public bool isGameOver;
+    }
+
+    // public static unsafe void PowerTwo(int* value)
+    // {
+    //     *value *= *value;
+    // }
+}
