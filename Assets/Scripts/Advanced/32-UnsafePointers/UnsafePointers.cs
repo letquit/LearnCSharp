92a7a795deedd77123e55888517a5262921e5fd6
letquit <1446444589@qq.com>
2026-02-06 21:36:00 +0800

    Unsafe Pointers

--- Assets/Scripts/Advanced/32-UnsafePointers/UnsafePointers.cs
+++ Assets/Scripts/Advanced/32-UnsafePointers/UnsafePointers.cs
@@ -1,37 +1,37 @@
 using System;
 using UnityEngine;
 
-// public unsafe class UnsafePointers : MonoBehaviour
-public class UnsafePointers : MonoBehaviour
+public unsafe class UnsafePointers : MonoBehaviour
+// public class UnsafePointers : MonoBehaviour
 {
-    // private unsafe void Start()
-    private void Start()
+    private unsafe void Start()
+    // private void Start()
     {
-        // unsafe
+        unsafe
         {
-            // int* intArrayPtr = stackalloc int[10];
-            // intArrayPtr += 5;
-            // // for (int i = 0; i < 20; i++)
-            // for (int i = 0; i < 10; i++)
-            // {
-            //     Debug.Log(intArrayPtr[i]);
-            // }
-            // Debug.Log(((IntPtr)intArrayPtr).ToString("x"));
-            //
-            // GameData gameData = new GameData();
-            // GameData* gameDataPtr = &gameData;
-            // gameDataPtr->coins = 12;
-            // gameDataPtr->isGameOver = false;
-            //
-            // int a = 5;
-            // PowerTwo(&a);
-            // Debug.Log(a);
-            //
-            // byte[] bytes = new byte[] { 1, 2, 3, 4, 5 };
-            // fixed (byte* bytePtr = bytes)
-            // {
-            //     
-            // }
+            int* intArrayPtr = stackalloc int[10];
+            intArrayPtr += 5;
+            // for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 10; i++)
+            {
+                Debug.Log(intArrayPtr[i]);
+            }
+            Debug.Log(((IntPtr)intArrayPtr).ToString("x"));
+            
+            GameData gameData = new GameData();
+            GameData* gameDataPtr = &gameData;
+            gameDataPtr->coins = 12;
+            gameDataPtr->isGameOver = false;
+            
+            int a = 5;
+            PowerTwo(&a);
+            Debug.Log(a);
+            
+            byte[] bytes = new byte[] { 1, 2, 3, 4, 5 };
+            fixed (byte* bytePtr = bytes)
+            {
+                
+            }
         }
     }
 
@@ -41,8 +41,8 @@ public class UnsafePointers : MonoBehaviour
         public bool isGameOver;
     }
 
-    // public static unsafe void PowerTwo(int* value)
-    // {
-    //     *value *= *value;
-    // }
+    public static unsafe void PowerTwo(int* value)
+    {
+        *value *= *value;
+    }
 }
---
+++ Assets/Scripts/Advanced/33-AsyncAwaitTask/AsyncAwaitTask.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class AsyncAwaitTask : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+}
